044270ff7473605a0f0d62273cecac3338175202
vanithabommadi <bommadivanitha@gmail.com>
2025-01-03 09:40:13 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,62 @@
+let questions = [
+    {
+        numb: 1,
+        question:"what does HTML stand for?",
+        answer:"c.Hyper Text Markup Language",
+        options:[
+         "A.Hyper Type Multi Language ",                
+         "B.Hyper Text Multiple Language",
+         "c.Hyper Text Markup Language",
+         "D.Hyper Text Multi Language",
+
+        ]
+    },
+
+{
+    numb: 2,
+    question:"what does CSS stand for?",
+    answer:"B.Cascading Style Sheets",
+    options:[
+        "A.Creative style sheets",
+        "B.Cascading Style Sheets",
+        "c.computer style sheets",
+        "D.custom style sheets" 
+    ]
+},
+{
+    numb: 3,
+    question:"Which programming language is mainly used for adding interactivity to websites?",
+    answer:"D) JavaScript",
+    options:[
+        "A) HTML",
+        "B) CSS",
+        "C) Python",
+        "D) JavaScript"
+        
+    ]
+},
+{
+    numb: 4,
+    question:"Which part of web development is responsible for handling data storage and retrieval?",
+    answer:"B) Back-end development",
+    options:[
+        "A) Front-end development",
+        "B) Back-end development",
+        "C) Full-stack development",
+        "D) Middleware development"
+        
+    ]
+},
+{
+    numb: 5,
+    question:"Which technology is primarily responsible for the styling of web pages?",
+    answer:"C) CSS",
+    options:[
+        "A) JavaScript",
+        "B) HTML",
+        "C) CSS",
+        "D) Python"
+        
+    ]
+},
+];  
